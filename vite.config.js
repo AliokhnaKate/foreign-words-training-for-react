d2d1c7104175733ab693470975b5b92c46dc1a4d
AliokhnaKate <159958135+AliokhnaKate@users.noreply.github.com>
2026-03-01 20:56:40 +0300

    Fix Vite configuration and build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/foreign-words-training-for-react/',
   plugins: [react()],
+  base: '/foreign-words-training-for-react/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
